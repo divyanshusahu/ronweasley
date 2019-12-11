8fdb0604ecb6a272318597199b2d0a210d235912
divyanshusahu <dsahu1997@gmail.com>
2019-12-11 11:36:38 +0530

    now this devlopment switched to other branch in favour of next.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const sls = require("serverless-http");
 
 const app = express();
 
@@ -19,6 +20,8 @@ if (true) {
   });
 }
 
-const port = process.env.PORT || 5000;
+//const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+//app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports.server = sls(app);
