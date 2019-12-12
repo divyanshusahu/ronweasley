3337500a73497953e10b3de32df94bc30929a8f7
divyanshusahu <dsahu1997@gmail.com>
2019-12-12 19:27:30 +0530

    add post toolbar modified for small screen devices

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,16 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import Input from "@material-ui/core/Input";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Icon from "@material-ui/core/Icon";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
+import Hidden from "@material-ui/core/Hidden";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { Link, Element } from "react-scroll";
 
@@ -21,7 +23,16 @@ import ScrollAnimation from "../utils/ScrollAnimation";
 import Footer from "../utils/Footer";
 import PostEditor from "../utils/PostEditor";
 
+const useStyles = new makeStyles(theme => ({
+  select: {
+    marginLeft: theme.spacing(2),
+    width: "200px"
+  }
+}));
+
 function Index() {
+  const classes = useStyles();
+
   const [selectedTab, setSelectedTab] = React.useState("Appericiation");
   const [tab_value, setTabValue] = React.useState(0);
   const tab_handlechange = (event, value) => {
@@ -33,6 +44,18 @@ function Index() {
     } else if (value === 2) {
       setSelectedTab("Fanart");
     } else if (value === 3) {
+      setSelectedTab("Fanfiction");
+    }
+  };
+  const handleSelectTabChange = event => {
+    setTabValue(event.target.value);
+    if (event.target.value === 0) {
+      setSelectedTab("Appreciation");
+    } else if (event.target.value === 1) {
+      setSelectedTab("Defense");
+    } else if (event.target.value === 2) {
+      setSelectedTab("Fanart");
+    } else if (event.target.value === 3) {
       setSelectedTab("Fanfiction");
     }
   };
@@ -65,9 +88,7 @@ function Index() {
               <p className="landing_big_heading">
                 A Website dedicated to the most selfless Harry Potter Character.
               </p>
-              <p className="landing_about">
-                Some lines best describing Ron.
-              </p>
+              <p className="landing_about">Some lines best describing Ron.</p>
               <Link to="landingContent" spy={true} smooth={true} duration={500}>
                 <ScrollAnimation />
               </Link>
@@ -77,42 +98,49 @@ function Index() {
         <Element name="landingContent">
           <div className="landing_content">
             <Container>
-              <Paper className="landing_content_paper" elevation={24}>
+              <Paper elevation={24}>
                 <div className="toolbar">
                   <Grid container>
-                    <Grid container item xs={12} lg={8}>
-                      <Tabs
-                        value={tab_value}
-                        onChange={tab_handlechange}
-                        indicatorColor="primary"
-                        textColor="primary"
-                        variant="scrollable"
-                        scrollButtons="auto"
-                      >
-                        <Tab label="Appreciation"></Tab>
-                        <Tab label="Defense"></Tab>
-                        <Tab label="Fanart"></Tab>
-                        <Tab label="Fanfiction"></Tab>
-                      </Tabs>
+                    <Grid container item xs={9} lg={10}>
+                      <Hidden mdDown>
+                        <Tabs
+                          value={tab_value}
+                          onChange={tab_handlechange}
+                          indicatorColor="primary"
+                          textColor="primary"
+                          variant="scrollable"
+                          scrollButtons="auto"
+                        >
+                          <Tab label="Appreciation"></Tab>
+                          <Tab label="Defense"></Tab>
+                          <Tab label="Fanart"></Tab>
+                          <Tab label="Fanfiction"></Tab>
+                        </Tabs>
+                      </Hidden>
+                      <Hidden lgUp>
+                        <FormControl
+                          variant="outlined"
+                          className={classes.select}
+                        >
+                          <Select
+                            value={tab_value}
+                            onChange={handleSelectTabChange}
+                          >
+                            <MenuItem value={0}>Appereciation</MenuItem>
+                            <MenuItem value={1}>Defense</MenuItem>
+                            <MenuItem value={2}>Fanart</MenuItem>
+                            <MenuItem value={3}>Fanfiction</MenuItem>
+                          </Select>
+                        </FormControl>
+                      </Hidden>
                     </Grid>
-                    <Grid container item xs={12} lg={4}>
-                      <Input
-                        startAdornment={
-                          <InputAdornment position="start">
-                            <Icon>search</Icon>
-                          </InputAdornment>
-                        }
-                        placeholder="Search"
-                      />
-                      <Button
-                        variant="outlined"
-                        color="primary"
-                        size="small"
-                        className="new_button"
+                    <Grid container item xs={3} lg={2}>
+                      <button
                         onClick={handleCreatePostOpen}
+                        className="new_button"
                       >
-                        New
-                      </Button>
+                        NEW
+                      </button>
                     </Grid>
                   </Grid>
                 </div>
@@ -167,6 +195,7 @@ function Index() {
             font-size: 3rem;
             margin-bottom: 0.35em;
             font-weight: 400;
+            padding: 8px;
           }
           .landing_about {
             color: #efefef;
@@ -175,9 +204,10 @@ function Index() {
             font-family: "Roboto";
             font-weight: 500;
             line-height: 1.6;
+            padding: 8px;
           }
           .landing_content {
-            padding: 80px 0;
+            padding: 40px 0 80px 0;
             background-image: linear-gradient(
               to top,
               #d5d4d0 0%,
@@ -187,15 +217,22 @@ function Index() {
               #e9e9e7 100%
             );
           }
-          .landing_content_paper {
-            min-height: 150vh;
-          }
           .toolbar {
             padding-top: 32px;
             min-height: 80px;
           }
           .new_button {
             margin-left: 16px;
+            padding: 16px;
+            background: none;
+            cursor: pointer;
+            border: none;
+            font-family: "Roboto";
+            border: 1px solid rgba(100, 100, 100, 0.5);
+            border-radius: 4px;
+          }
+          .new_button:focus {
+            outline: 0;
           }
           .landing_posts {
             min-height: calc(150vh - 80px);
