3133297a441da4743220855c36b5696b42b9e94e
divyanshusahu <dsahu1997@gmail.com>
2019-12-25 18:48:04 +0530

    navigation bar made material ui free

--- frontend/utils/NavigationBar.js
+++ frontend/utils/NavigationBar.js
@@ -3,32 +3,11 @@ import Link from "next/link";
 
 import clsx from "clsx";
 
-import Container from "@material-ui/core/Container";
-import Hidden from "@material-ui/core/Hidden";
-import Popper from "@material-ui/core/Popper";
-import Fade from "@material-ui/core/Fade";
-import Icon from "@material-ui/core/Icon";
-import Drawer from "@material-ui/core/Drawer";
+import { Row, Col, Icon, Drawer, Menu, Dropdown } from "antd";
+
+const { SubMenu, Item } = Menu;
 
 function NavigationBar(props) {
-  const [friendshipElement, setFriendshipElement] = React.useState(null);
-  const handleFriendshipOpen = event => {
-    setFriendshipElement(event.currentTarget);
-  };
-  const handleFriendshipClose = () => {
-    setFriendshipElement(null);
-  };
-  const friendship = Boolean(friendshipElement);
-
-  const [fanonshipElement, setFanonshipElement] = React.useState(null);
-  const handleFanonshipOpen = event => {
-    setFanonshipElement(event.currentTarget);
-  };
-  const handleFanonshipClose = () => {
-    setFanonshipElement(null);
-  };
-  const fanonship = Boolean(fanonshipElement);
-
   const [drawerOpen, setDrawerOpen] = React.useState(false);
   const handleDrawerOpen = () => {
     setDrawerOpen(true);
@@ -37,23 +16,95 @@ function NavigationBar(props) {
     setDrawerOpen(false);
   };
 
-  const [nestedFriendships, setNestedFriendships] = React.useState(true);
-  const [nestedFanonShips, setNestedFanonShips] = React.useState(true);
+  const FriendshipsMenu = (
+    <ul style={{ listStyle: "none", backgroundColor: "#fff" }}>
+      <li>
+        <p
+          style={{
+            fontSize: "16px",
+            lineHeight: "64px",
+            fontFamily: "Capriola",
+            color: "#000",
+            marginBottom: 0,
+            border: "1px solid rgba(128,128,128,0.25)",
+            borderTop: "none"
+          }}
+        >
+          Golden Trio
+        </p>
+      </li>
+      <li>
+        <p
+          style={{
+            fontSize: "16px",
+            lineHeight: "64px",
+            fontFamily: "Capriola",
+            color: "#000",
+            marginBottom: 0,
+            border: "1px solid rgba(128,128,128,0.25)",
+            borderTop: "none"
+          }}
+        >
+          Weasley Family
+        </p>
+      </li>
+    </ul>
+  );
 
-  const handleNestedFunction = type => {
-    if (type === "fs") {
-      setNestedFriendships(!nestedFriendships);
-    } else if (type === "fss") {
-      setNestedFanonShips(!nestedFanonShips);
-    }
-  };
+  const FanonshipMenu = (
+    <ul style={{ listStyle: "none", backgroundColor: "#fff" }}>
+      <li>
+        <p
+          style={{
+            fontSize: "16px",
+            lineHeight: "64px",
+            fontFamily: "Capriola",
+            color: "#000",
+            marginBottom: 0,
+            border: "1px solid rgba(128,128,128,0.25)",
+            borderTop: "none"
+          }}
+        >
+          Ron-Lavender
+        </p>
+      </li>
+      <li>
+        <p
+          style={{
+            fontSize: "16px",
+            lineHeight: "64px",
+            fontFamily: "Capriola",
+            color: "#000",
+            marginBottom: 0,
+            border: "1px solid rgba(128,128,128,0.25)",
+            borderTop: "none"
+          }}
+        >
+          Ron-Harry
+        </p>
+      </li>
+      <li>
+        <p
+          style={{
+            fontSize: "16px",
+            lineHeight: "64px",
+            fontFamily: "Capriola",
+            color: "#000",
+            marginBottom: 0,
+            border: "1px solid rgba(128,128,128,0.25)",
+            borderTop: "none"
+          }}
+        >
+          Ron-Luna
+        </p>
+      </li>
+    </ul>
+  );
 
   return (
     <div
       className={clsx({
         dark: props.dark,
-        sticky_navigation: true,
-        initial_navigation: !drawerOpen,
         light: props.light
       })}
     >
@@ -67,205 +118,129 @@ function NavigationBar(props) {
           rel="stylesheet"
         />
       </Head>
-      <Container maxWidth="xl">
-        <div className="navigation">
-          <div className="navigation_logo">
-            <Link href="/">
-              <a>
-                <p className="navigation_logo_text">King Weasley</p>
-              </a>
-            </Link>
-          </div>
-          <Hidden mdDown>
-            <ul className="navigation_links">
-              <li
-                className="navigation_items"
-                aria-owns={friendship ? "friendship-popper" : undefined}
-                aria-haspopup="true"
-                onMouseEnter={handleFriendshipOpen}
-                onMouseLeave={handleFriendshipClose}
-              >
-                <p className="navigation_text">Friendships</p>
-                <Popper
-                  id="friendship-popper"
-                  open={friendship}
-                  anchorEl={friendshipElement}
-                  disablePortal={true}
-                  placement="bottom-start"
-                  transition
-                  style={{ zIndex: "1300" }}
-                >
-                  {({ TransitionProps }) => (
-                    <Fade {...TransitionProps} timeout={250}>
-                      <div className="popper_element">
-                        <ul className="popper_list">
-                          <li
-                            className={clsx({
-                              popper_list_items: true,
-                              custom_borders: props.light
-                            })}
-                          >
-                            Golden Trio
-                          </li>
-                          <li
-                            className={clsx({
-                              popper_list_items: true,
-                              custom_borders: props.light
-                            })}
-                          >
-                            Weasley Family
-                          </li>
-                        </ul>
-                      </div>
-                    </Fade>
-                  )}
-                </Popper>
-              </li>
-              <li className="navigation_items">
-                <Link href="/romione">
+      <div>
+        <Row>
+          <Col xs={{ span: 22, offset: 1 }}>
+            <div className="navigation">
+              <div>
+                <Link href="/">
                   <a>
-                    <p className="navigation_text">Romione</p>
+                    <p className="navigation_logo_text">King Weasley</p>
                   </a>
                 </Link>
-              </li>
-              <li
-                className="navigation_items"
-                aria-owns={fanonship ? "fanonship-popper" : undefined}
-                aria-haspopup="true"
-                onMouseEnter={handleFanonshipOpen}
-                onMouseLeave={handleFanonshipClose}
-              >
-                <p className="navigation_text">Fanon Ships</p>
-                <Popper
-                  id="fanonship-popper"
-                  open={fanonship}
-                  anchorEl={fanonshipElement}
-                  disablePortal={true}
-                  placement="bottom-start"
-                  transition
-                  style={{ zIndex: "1300" }}
-                >
-                  {({ TransitionProps }) => (
-                    <Fade {...TransitionProps} timeout={250}>
-                      <div className="popper_element">
-                        <ul className="popper_list">
-                          <li
-                            className={clsx({
-                              popper_list_items: true,
-                              custom_borders: props.light
-                            })}
-                          >
-                            <span>Ron-Lavender</span>
-                          </li>
-                          <li
-                            className={clsx({
-                              popper_list_items: true,
-                              custom_borders: props.light
-                            })}
-                          >
-                            <span>Ron-Harry</span>
-                          </li>
-                          <li
-                            className={clsx({
-                              popper_list_items: true,
-                              custom_borders: props.light
-                            })}
-                          >
-                            <span>Ron-Luna</span>
-                          </li>
-                        </ul>
-                      </div>
-                    </Fade>
-                  )}
-                </Popper>
-              </li>
-              <li className="navigation_items">
-                <Link href="suggestions">
-                  <a>
-                    <p className="navigation_text">Suggestions</p>
-                  </a>
-                </Link>
-              </li>
-              <li className="navigation_items">
-                <p className="navigation_text">Credits</p>
-              </li>
-            </ul>
-          </Hidden>
-          <Hidden lgUp>
-            <Icon fontSize="large" onClick={handleDrawerOpen}>
-              menu
-            </Icon>
-            <Drawer open={drawerOpen} onClose={handleDrawerClose}>
-              <div className="drawer">
-                <div className="drawer_main">
-                  <Link href="/">
-                    <a>
-                      <p className="drawer_heading">King Weasley</p>
-                    </a>
-                  </Link>
-                  <Icon onClick={handleDrawerClose}>close</Icon>
-                </div>
-                <ul>
-                  <li
-                    onClick={() => handleNestedFunction("fs")}
-                    className="drawer_list_items nested_list"
-                  >
-                    <span>Friendships</span>
-                    {nestedFriendships ? (
-                      <Icon>expand_more</Icon>
-                    ) : (
-                      <Icon>expand_less</Icon>
-                    )}
-                  </li>
-                  <ul className={clsx({ hidden_class: nestedFriendships })}>
-                    <li className="drawer_list_items nested_list_items">
-                      Golden Trio
-                    </li>
-                    <li className="drawer_list_items nested_list_items">
-                      Weasley Family
-                    </li>
-                  </ul>
-                  <Link href="/romione">
-                    <a>
-                      <li className="drawer_list_items">Romione</li>
-                    </a>
-                  </Link>
-                  <li
-                    onClick={() => handleNestedFunction("fss")}
-                    className="drawer_list_items nested_list"
-                  >
-                    <span>Fanon Ships</span>
-                    {nestedFanonShips ? (
-                      <Icon>expand_more</Icon>
-                    ) : (
-                      <Icon>expand_less</Icon>
-                    )}
-                  </li>
-                  <ul className={clsx({ hidden_class: nestedFanonShips })}>
-                    <li className="drawer_list_items nested_list_items">
-                      Ron-Lavender
-                    </li>
-                    <li className="drawer_list_items nested_list_items">
-                      Ron-Harry
-                    </li>
-                    <li className="drawer_list_items nested_list_items">
-                      Ron-Luna
-                    </li>
-                  </ul>
-                  <Link href="/suggestions">
-                    <a>
-                      <li className="drawer_list_items">Suggestions</li>
-                    </a>
-                  </Link>
-                  <li className="drawer_list_items">Credits</li>
-                </ul>
               </div>
-            </Drawer>
-          </Hidden>
-        </div>
-      </Container>
+              <div>
+                <div className="xl_up">
+                  <ul className="navigation_links">
+                    <li className="navigation_item">
+                      <Dropdown
+                        placement="bottomCenter"
+                        overlay={FriendshipsMenu}
+                        trigger={["hover", "click"]}
+                        overlayStyle={{ width: "150px" }}
+                        getPopupContainer={() =>
+                          document.getElementsByClassName("navigation_item")[0]
+                        }
+                      >
+                        <p className="navigation_text">Friendships</p>
+                      </Dropdown>
+                    </li>
+                    <li className="navigation_item">
+                      <Link href="/romione">
+                        <a>
+                          <p className="navigation_text">Romione</p>
+                        </a>
+                      </Link>
+                    </li>
+                    <li className="navigation_item">
+                      <Dropdown
+                        placement="bottomCenter"
+                        overlay={FanonshipMenu}
+                        trigger={["hover", "click"]}
+                        overlayStyle={{ width: "150px" }}
+                        getPopupContainer={() =>
+                          document.getElementsByClassName("navigation_item")[2]
+                        }
+                      >
+                        <p className="navigation_text">Fanon Ships</p>
+                      </Dropdown>
+                    </li>
+                    <li className="navigation_item">
+                      <Link href="/suggestions">
+                        <a>
+                          <p className="navigation_text">Suggestions</p>
+                        </a>
+                      </Link>
+                    </li>
+                    <li className="navigation_item">
+                      <p className="navigation_text">Credits</p>
+                    </li>
+                  </ul>
+                </div>
+                <div className="xl_down">
+                  <Icon
+                    type="menu"
+                    style={{ fontSize: "20px" }}
+                    onClick={handleDrawerOpen}
+                  />
+                  <Drawer
+                    title={
+                      <Link href="/">
+                        <a>King Weasley</a>
+                      </Link>
+                    }
+                    placement="left"
+                    visible={drawerOpen}
+                    onClose={handleDrawerClose}
+                    destroyOnClose
+                  >
+                    <Menu mode="inline" style={{ border: "none" }}>
+                      <SubMenu title="Friendships">
+                        <Item>Golden Trio</Item>
+                        <Item>Weasley Family</Item>
+                      </SubMenu>
+                      <Item>
+                        <Link href="/romione">
+                          <a>Romione</a>
+                        </Link>
+                      </Item>
+                      <SubMenu title="Fanon Ships">
+                        <Item>Ron-Lavender</Item>
+                        <Item>Ron-Harry</Item>
+                        <Item>Ron-Luna</Item>
+                      </SubMenu>
+                      <Item>
+                        <Link href="/suggestions">
+                          <a>Suggestions</a>
+                        </Link>
+                      </Item>
+                      <Item>Credits</Item>
+                    </Menu>
+                  </Drawer>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </div>
       <style jsx>
         {`
+          @media only screen and (max-width: 1199px) {
+            .xl_down {
+              display: block;
+            }
+            .xl_up {
+              display: none;
+            }
+          }
+          @media only screen and (min-width: 1200px) {
+            .xl_down {
+              display: none;
+            }
+            .xl_up {
+              display: block;
+            }
+          }
           .dark {
             color: #ffffff;
           }
@@ -274,100 +249,39 @@ function NavigationBar(props) {
             color: #000000;
             box-shadow: 0 2px 4px 0 rgba(128, 128, 128, 1);
           }
-          .initial_navigation {
-            z-index: 10000;
-          }
-          .sticky_navigation {
-            position: sticky;
-            top: 0;
-          }
           .navigation {
-            height: 8vh;
+            height: 64px;
             display: flex;
             justify-content: space-between;
             align-items: center;
             text-transform: uppercase;
           }
-          .navigation_logo {
-            cursor: pointer;
-            height: 100%;
+          .navigation_logo_text {
+            line-height: 64px;
+            font-size: 24px;
+            font-family: "Shojumaru";
+            margin-bottom: 0;
           }
           .navigation_links {
             list-style: none;
             display: flex;
-            height: 100%;
+            margin-bottom: 0;
           }
-          .navigation_items {
+          .navigation_item {
             cursor: pointer;
-            width: 180px;
+            width: 150px;
             text-align: center;
-            transition: 0.25s;
+            transition: 0.5s;
           }
-          .navigation_items:hover {
+          .navigation_item:hover {
             color: #000;
             background-color: #fff;
-            border-bottom: 1px solid rgba(100, 100, 100, 0.25);
-          }
-          .navigation_logo_text {
-            line-height: 8vh;
-            font-size: 24px;
-            font-family: "Shojumaru";
           }
           .navigation_text {
-            line-height: 8vh;
+            line-height: 64px;
             font-size: 16px;
             font-family: "Capriola";
-          }
-          .popper_element {
-            width: 180px;
-            background-color: #fff;
-            text-align: left;
-          }
-          .popper_list {
-            list-style: none;
-          }
-          .popper_list_items {
-            font-size: 16px;
-            padding: 16px;
-            font-family: "Capriola";
-            border-bottom: 1px solid rgba(100, 100, 100, 0.25);
-          }
-          .custom_borders {
-            border-left: 1px solid rgba(100, 100, 100, 0.25);
-            border-right: 1px solid rgba(100, 100, 100, 0.25);
-          }
-          .drawer {
-            width: 80vw;
-            min-height: 100%;
-            background-image: linear-gradient(-20deg, #e9defa 0%, #fbfcdb 100%);
-            text-transform: uppercase;
-            font-family: "Capriola";
-          }
-          .hidden_class {
-            display: none;
-          }
-          .drawer_main {
-            width: 100%;
-            height: 80px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 0 8px;
-            border-bottom: 1px solid rgba(100, 100, 100, 0.25);
-          }
-          .drawer_heading {
-            font-size: 24px;
-          }
-          .drawer_list_items {
-            padding: 12px;
-          }
-          .nested_list {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-          }
-          .nested_list_items {
-            padding-left: 36px;
+            margin-bottom: 0;
           }
         `}
       </style>
