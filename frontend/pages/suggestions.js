ef1496655ec482a7769441b84a4fdc947d228476
divyanshusahu <dsahu1997@gmail.com>
2019-12-28 00:48:54 +0530

    suggestions page integrated with backend

--- frontend/pages/suggestions.js
+++ frontend/pages/suggestions.js
@@ -12,38 +12,69 @@ import {
   Input,
   Comment,
   Avatar,
-  Empty
+  Empty,
+  message
 } from "antd";
 
 import SecondaryLayout from "../components/SecondaryLayout";
 
-function Suggestions() {
-  const BASE_URL =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:5000"
-      : "https://api.ronweasley.co";
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://api.ronweasley.co";
 
-  const [bugs, setBugs] = React.useState([]);
-  const [suggestions, setSuggestions] = React.useState([]);
-  const [feedbacks, setFeedbacks] = React.useState([]);
+function Suggestions({ pageLoadBugs, pageLoadSuggestions, pageLoadFeedbacks }) {
+  const [bugs, setBugs] = React.useState(pageLoadBugs);
+  const [suggestions, setSuggestions] = React.useState(pageLoadSuggestions);
+  const [feedbacks, setFeedbacks] = React.useState(pageLoadFeedbacks);
 
-  React.useEffect(() => {
-    fetch(BASE_URL + "/suggestions/get/suggestion")
+  const [bugInputValue, setBugInputValue] = React.useState("");
+  const [suggestionInputValue, setSuggestionInputValue] = React.useState("");
+  const [feedbackInputValue, setFeedbackInputValue] = React.useState("");
+
+  const refreshFeed = type => {
+    fetch(BASE_URL + "/suggestions/get/" + type)
       .then(r => r.json())
       .then(data => {
         if (data.success) {
-          setSuggestions(data.posts);
+          if (type === "bug") {
+            setBugs(data.posts);
+            setBugInputValue("");
+          } else if (type === "suggestion") {
+            setSuggestions(data.posts);
+            setSuggestionInputValue("");
+          } else if (type === "feedback") {
+            setFeedbacks(data.posts);
+            setFeedbackInputValue("");
+          }
         }
       });
+  };
 
-    fetch(BASE_URL + "/suggestions/get/feedback")
+  const post_user_feedback = type => {
+    let post_data = {
+      post_type: type,
+      content: document.getElementById(`input-${type}`).value
+    };
+    message.loading({
+      content: "Action in progress...",
+      key: "handlePostLoading"
+    });
+    fetch(BASE_URL + "/suggestions/new", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(post_data)
+    })
       .then(r => r.json())
       .then(data => {
         if (data.success) {
-          setFeedbacks(data.posts);
+          message.success({ content: data.message, key: "handlePostLoading" });
+          refreshFeed(type);
+        } else {
+          message.error({ content: data.message, key: "handlePostLoading" });
         }
       });
-  }, []);
+  };
 
   return (
     <div>
@@ -62,32 +93,34 @@ function Suggestions() {
                 }
                 extra={<Icon type="warning" style={{ fontSize: 20 }} />}
               >
-                <Collapse bordered={false}>
-                  <Collapse.Panel
-                    header="Android, IOS App?"
-                    key={1}
-                    extra={
-                      <div>
-                        <Icon
-                          type="like"
-                          onClick={event => event.stopPropagation()}
-                        />
-                        <span style={{ paddingLeft: 8, paddingRight: 8 }}>
-                          0
-                        </span>
-                        <Icon
-                          type="dislike"
-                          onClick={event => event.stopPropagation()}
-                        />
-                        <span style={{ paddingLeft: 8 }}>0</span>
-                      </div>
-                    }
-                  >
-                    <Typography.Text>
-                      Is Android, IOS app needed?
-                    </Typography.Text>
-                  </Collapse.Panel>
-                </Collapse>
+                <div className="admin_feedback_card_content">
+                  <Collapse bordered={false}>
+                    <Collapse.Panel
+                      header="Android, IOS App?"
+                      key={1}
+                      extra={
+                        <div>
+                          <Icon
+                            type="like"
+                            onClick={event => event.stopPropagation()}
+                          />
+                          <span style={{ paddingLeft: 8, paddingRight: 8 }}>
+                            0
+                          </span>
+                          <Icon
+                            type="dislike"
+                            onClick={event => event.stopPropagation()}
+                          />
+                          <span style={{ paddingLeft: 8 }}>0</span>
+                        </div>
+                      }
+                    >
+                      <Typography.Text>
+                        Is Android, IOS app needed?
+                      </Typography.Text>
+                    </Collapse.Panel>
+                  </Collapse>
+                </div>
               </Card>
             </Col>
             <Col xs={{ span: 22, offset: 1 }} md={{ span: 9, offset: 2 }}>
@@ -103,7 +136,11 @@ function Suggestions() {
                   </span>
                 }
                 extra={<Icon type="check-square" style={{ fontSize: 20 }} />}
-              ></Card>
+              >
+                <div className="admin_feedback_card_content">
+                  <Empty />
+                </div>
+              </Card>
             </Col>
           </Row>
         </div>
@@ -122,13 +159,48 @@ function Suggestions() {
                 extra={<Icon type="bug" style={{ fontSize: 20 }} />}
                 actions={[
                   <Input
+                    id="input-bug"
                     placeholder="Report Bug here"
-                    addonAfter={<Icon type="enter" />}
+                    addonAfter={
+                      <Icon
+                        type="enter"
+                        onClick={() => post_user_feedback("bug")}
+                      />
+                    }
+                    onPressEnter={() => post_user_feedback("bug")}
+                    value={bugInputValue}
+                    onChange={e => setBugInputValue(e.target.value)}
                   />
                 ]}
               >
                 <div className="user_feedback_card_content">
-                  {isEmpty(bugs) ? <Empty /> : null}
+                  {isEmpty(bugs) ? (
+                    <Empty />
+                  ) : (
+                    bugs.map(b => (
+                      <Comment
+                        content={b.content["S"]}
+                        datetime={<TimeAgo date={b.post_date["S"]} />}
+                        key={b.post_id["S"]}
+                        author="Anon"
+                        avatar={<Avatar icon="bug" />}
+                      >
+                        {isEmpty(b.reply) ? null : (
+                          <Comment
+                            content={b.reply["S"]}
+                            author="Admin"
+                            datetime={b.reply_time["S"]}
+                            avatar={
+                              <Avatar
+                                icon="user"
+                                style={{ backgroundColor: "#52c41a" }}
+                              />
+                            }
+                          />
+                        )}
+                      </Comment>
+                    ))
+                  )}
                 </div>
               </Card>
             </Col>
@@ -145,40 +217,53 @@ function Suggestions() {
                 extra={<Icon type="question-circle" style={{ fontSize: 20 }} />}
                 actions={[
                   <Input
+                    id="input-suggestion"
                     placeholder="Enter suggestions here"
-                    addonAfter={<Icon type="enter" />}
+                    addonAfter={
+                      <Icon
+                        type="enter"
+                        onClick={() => post_user_feedback("suggestion")}
+                      />
+                    }
+                    onPressEnter={() => post_user_feedback("suggestion")}
+                    value={suggestionInputValue}
+                    onChange={e => setSuggestionInputValue(e.target.value)}
                   />
                 ]}
               >
                 <div className="user_feedback_card_content">
-                  {suggestions.map(sug => (
-                    <Comment
-                      content={sug.content["S"]}
-                      datetime={<TimeAgo date={sug.post_date["S"]} />}
-                      key={sug.post_id["S"]}
-                      author="Anon"
-                      avatar={
-                        <Avatar
-                          icon="user"
-                          style={{ backgroundColor: "#f5222d" }}
-                        />
-                      }
-                    >
-                      {isEmpty(sug.reply) ? null : (
-                        <Comment
-                          content={sug.reply["S"]}
-                          author="Admin"
-                          datetime={sug.reply_time["S"]}
-                          avatar={
-                            <Avatar
-                              icon="user"
-                              style={{ backgroundColor: "#52c41a" }}
-                            />
-                          }
-                        />
-                      )}
-                    </Comment>
-                  ))}
+                  {isEmpty(suggestions) ? (
+                    <Empty />
+                  ) : (
+                    suggestions.map(sug => (
+                      <Comment
+                        content={sug.content["S"]}
+                        datetime={<TimeAgo date={sug.post_date["S"]} />}
+                        key={sug.post_id["S"]}
+                        author="Anon"
+                        avatar={
+                          <Avatar
+                            icon="user"
+                            style={{ backgroundColor: "#f5222d" }}
+                          />
+                        }
+                      >
+                        {isEmpty(sug.reply) ? null : (
+                          <Comment
+                            content={sug.reply["S"]}
+                            author="Admin"
+                            datetime={sug.reply_time["S"]}
+                            avatar={
+                              <Avatar
+                                icon="user"
+                                style={{ backgroundColor: "#52c41a" }}
+                              />
+                            }
+                          />
+                        )}
+                      </Comment>
+                    ))
+                  )}
                 </div>
               </Card>
             </Col>
@@ -197,21 +282,34 @@ function Suggestions() {
                 }
                 actions={[
                   <Input
+                    id="input-feedback"
                     placeholder="Leave feedback here"
-                    addonAfter={<Icon type="enter" />}
+                    addonAfter={
+                      <Icon
+                        type="enter"
+                        onClick={() => post_user_feedback("feedback")}
+                      />
+                    }
+                    onPressEnter={() => post_user_feedback("feedback")}
+                    value={feedbackInputValue}
+                    onChange={e => setFeedbackInputValue(e.target.value)}
                   />
                 ]}
               >
                 <div className="user_feedback_card_content">
-                  {feedbacks.map(fed => (
-                    <Comment
-                      content={fed.content["S"]}
-                      datetime={<TimeAgo date={fed.post_date["S"]} />}
-                      key={fed.post_id["S"]}
-                      author="Anon"
-                      avatar={<Avatar icon="user" />}
-                    />
-                  ))}
+                  {isEmpty(feedbacks) ? (
+                    <Empty />
+                  ) : (
+                    feedbacks.map(fed => (
+                      <Comment
+                        content={fed.content["S"]}
+                        datetime={<TimeAgo date={fed.post_date["S"]} />}
+                        key={fed.post_id["S"]}
+                        author="Anon"
+                        avatar={<Avatar icon="user" />}
+                      />
+                    ))
+                  )}
                 </div>
               </Card>
             </Col>
@@ -227,6 +325,11 @@ function Suggestions() {
             margin-top: 32px;
             margin-bottom: 48px;
           }
+          .admin_feedback_card_content {
+            height: 320px;
+            max-height: 320px;
+            overflow: auto;
+          }
           .user_feedback_card_content {
             height: 280px;
             max-height: 280px;
@@ -237,5 +340,31 @@ function Suggestions() {
     </div>
   );
 }
+
+Suggestions.getInitialProps = async () => {
+  const r1 = await fetch(BASE_URL + "/suggestions/get/bug");
+  let pageLoadBugs = await r1.json();
+  if (pageLoadBugs.success) {
+    pageLoadBugs = pageLoadBugs.posts;
+  }
+
+  const r2 = await fetch(BASE_URL + "/suggestions/get/suggestion");
+  let pageLoadSuggestions = await r2.json();
+  if (pageLoadSuggestions.success) {
+    pageLoadSuggestions = pageLoadSuggestions.posts;
+  }
+
+  const r3 = await fetch(BASE_URL + "/suggestions/get/feedback");
+  let pageLoadFeedbacks = await r3.json();
+  if (pageLoadFeedbacks.success) {
+    pageLoadFeedbacks = pageLoadFeedbacks.posts;
+  }
+
+  return {
+    pageLoadBugs: pageLoadBugs,
+    pageLoadSuggestions: pageLoadSuggestions,
+    pageLoadFeedbacks: pageLoadFeedbacks
+  };
+};
 
 export default Suggestions;
