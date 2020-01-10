765f19106db48ec1c950ed51011264eca7f1d4dc
divyanshusahu <dsahu1997@gmail.com>
2020-01-10 12:14:15 +0530

    update post endpoint implemented

--- backend/api/post_handler/edit_post.py
+++ backend/api/post_handler/edit_post.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identi
 import boto3
 import os
 import hashlib
+import validators
 
 edit_post = Blueprint("edit_post", __name__, url_prefix="/edit_post")
 
@@ -59,3 +60,93 @@ def identity_check():
     if post_id == identity:
         return jsonify({"success": True}), 200
     return jsonify({"success": False, "message": "Unauthorized"}), 403
+
+
+@edit_post.route("/update/<post_type>/<post_id>", methods=["POST"])
+@jwt_required
+def update_post(post_type, post_id):
+    if request.is_json == False:
+        return jsonify({"success": False, "message": "Bad Request"}), 400
+
+    identity = get_jwt_identity()
+    if identity != post_id:
+        return jsonify({"success": False, "message": "Unauthorized"}), 403
+
+    post_title = request.json.get("post_title", "")
+    if len(post_title) == 0:
+        return jsonify({"success": False, "message": "Post Title must not be empty"}), 400
+    if validators.length(post_title, min=3) != True:
+        return jsonify({"success": False, "message": "Post title must be three characters long"}), 400
+
+    post_author = request.json.get("post_author", "")
+    if len(post_author) == 0:
+        post_author = "Anonymous"
+
+    post_author_link = request.json.get("post_author_link", "")
+    if len(post_author_link) and validators.url(post_author_link, public=True) != True:
+        return jsonify({"success": False, "message": "Url must be valid"}), 400
+    if len(post_author_link) == 0:
+        post_author_link = "/anonymous/anon.jpg"
+
+    post_content = request.json.get("post_content", "")
+    if len(post_content) == 0:
+        return jsonify({"success": False, "message": "Post content must not be empty"}), 400
+
+    post_secret = request.json.get("post_secret", "")
+
+    try:
+        db.update_item(
+            TableName=os.environ["POST_TABLE"],
+            Key={
+                "post_type": {
+                    "S": post_type
+                },
+                "post_id": {
+                    "S": post_id
+                }
+            },
+            ConditionExpression="post_type = :post_type AND post_id = :post_id",
+            UpdateExpression="SET post_title = :post_title, post_author = :post_author, post_author_link = :post_author_link, post_content = :post_content",
+            ExpressionAttributeValues={
+                ":post_type": {"S": post_type},
+                ":post_id": {"S": post_id},
+                ":post_title": {"S": post_title},
+                ":post_author": {"S": post_author},
+                ":post_author_link": {"S": post_author_link},
+                ":post_content": {"S": post_content}
+            }
+        )
+    except db.exceptions.ConditionalCheckFailedException:
+        return jsonify({"success": False, "message": "Post does not exist"}), 404
+
+    if len(post_secret) == 0:
+        return jsonify({"success": True, "message": "Successfully Updated"}), 200
+    else:
+        if validators.length(post_secret, min=6) != True:
+            return jsonify({"success": True, "message": "Post secret must be six characters long"}), 400
+
+        post_secret = hashlib.sha256(post_secret.encode()).hexdigest()
+        try:
+            db.update_item(
+                TableName=os.environ["POST_TABLE"],
+                Key={
+                    "post_type": {
+                        "S": post_type
+                    },
+                    "post_id": {
+                        "S": post_id
+                    }
+                },
+                ConditionExpression="post_type = :post_type AND post_id = :post_id",
+                UpdateExpression="SET post_secret = :post_secret",
+                ExpressionAttributeValues={
+                    ":post_type": {"S": post_type},
+                    ":post_id": {"S": post_id},
+                    ":post_secret": {"S": post_secret}
+                }
+            )
+            return jsonify({"success": True, "message": "Successfully Updated"}), 200
+        except db.exceptions.ConditionalCheckFailedException:
+            return jsonify({"success": False, "message": "Post does not exist"}), 404
+
+    return jsonify({"success": False, "message": "Bad Request"}), 400
