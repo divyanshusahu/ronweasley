e497e008ff774a5b0e321a63cf47fbfaadc06e63
divyanshusahu <dsahu1997@gmail.com>
2020-04-01 02:39:42 +0530

    credits data file created

---
+++ frontend/data/credits_data.js
@@ -0,0 +1,66 @@
+const credits_data = {
+  ron_weasley: [
+    {
+      author: "stopblushingemma",
+      author_link: "https://stopblushingemma.tumblr.com",
+      original_post:
+        "https://stopblushingemma.tumblr.com/post/137186406330/colourful-ginger-boy"
+    },
+    {
+      author: "hillyminne",
+      author_link: "https://hillyminne.tumblr.com",
+      original_post:
+        "https://hillyminne.tumblr.com/post/173183074502/bearded-ron-weasley-d-some-ladies-were"
+    },
+    {
+      author: "atalienart",
+      author_link: "https://atalienart.tumblr.com",
+      original_post: "https://atalienart.tumblr.com/post/156010477127/ron"
+    },
+    {
+      author: "upthehillart",
+      author_link: "https://upthehillart.tumblr.com",
+      original_post:
+        "https://upthehillart.tumblr.com/post/157845950310/pigwidgeon-the-king"
+    }
+  ],
+
+  romione: [
+    {
+      author: "uknow-who",
+      author_link: "https://www.deviantart.com/uknow-who",
+      original_post:
+        "https://www.deviantart.com/uknow-who/art/Is-this-the-moment-111625167"
+    },
+    {
+      author: "viria13",
+      author_link: "https://www.deviantart.com/viria13",
+      original_post:
+        "https://www.deviantart.com/viria13/art/asdsafsdfsdfsgsfg-268608931"
+    },
+    {
+      author: "hillyminne",
+      author_link: "https://hillyminne.tumblr.com",
+      original_post:
+        "https://hillyminne.tumblr.com/post/169165623847/for-callieskye-d-hope-you-enjoy-d-ron-and"
+    },
+    {
+      author: "lillywmw",
+      author_link: "https://www.deviantart.com/lillywmw",
+      original_post:
+        "https://www.deviantart.com/lillywmw/art/The-Good-Luck-Kiss-57668029"
+    }
+  ],
+
+  golden_trio: [],
+
+  weasley_family: [],
+
+  ron_and_harry: [],
+
+  ron_and_lavender: [],
+
+  ron_and_luna: []
+};
+
+export default credits_data;
