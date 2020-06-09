f4d710332447ec394a4b08663c0d71e70f878841
divyanshusahu <dsahu1997@gmail.com>
2020-06-09 13:54:09 +0530

    purgecss plugin added

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,31 +1,41 @@
+const withPlugins = require("next-compose-plugins");
 const withOffline = require("next-offline");
+const withCss = require("@zeit/next-css");
+const withPurgeCss = require("next-purgecss");
 
-module.exports = withOffline({
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? "service-worker.js"
-      : "static/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "offlineCache",
-          expiration: {
-            maxEntries: 200,
+module.exports = withPlugins([
+  withCss,
+  withPurgeCss,
+  [
+    withOffline,
+    {
+      workboxOpts: {
+        swDest: process.env.NEXT_EXPORT
+          ? "service-worker.js"
+          : "static/service-worker.js",
+        runtimeCaching: [
+          {
+            urlPattern: /^https?.*/,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "offlineCache",
+              expiration: {
+                maxEntries: 200,
+              },
+            },
           },
+        ],
+      },
+      experimental: {
+        async rewrites() {
+          return [
+            {
+              source: "/service-worker.js",
+              destination: "/_next/static/service-worker.js",
+            },
+          ];
         },
       },
-    ],
-  },
-  experimental: {
-    async rewrites() {
-      return [
-        {
-          source: "/service-worker.js",
-          destination: "/_next/static/service-worker.js",
-        },
-      ];
     },
-  },
-});
+  ],
+]);
