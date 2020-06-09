f6c45c2def8f54bfda61e1ab3c09c6241c22c340
divyanshusahu <dsahu1997@gmail.com>
2020-06-09 14:58:52 +0530

    zeit css plugin removed

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,41 +1,40 @@
 const withPlugins = require("next-compose-plugins");
 const withOffline = require("next-offline");
-const withCss = require("@zeit/next-css");
 const withPurgeCss = require("next-purgecss");
 
-module.exports = withPlugins([
-  withCss,
-  withPurgeCss,
-  [
-    withOffline,
-    {
-      workboxOpts: {
-        swDest: process.env.NEXT_EXPORT
-          ? "service-worker.js"
-          : "static/service-worker.js",
-        runtimeCaching: [
-          {
-            urlPattern: /^https?.*/,
-            handler: "NetworkFirst",
-            options: {
-              cacheName: "offlineCache",
-              expiration: {
-                maxEntries: 200,
-              },
-            },
+const offlineConfig = {
+  workboxOpts: {
+    swDest: process.env.NEXT_EXPORT
+      ? "service-worker.js"
+      : "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "offlineCache",
+          expiration: {
+            maxEntries: 200,
           },
-        ],
-      },
-      experimental: {
-        async rewrites() {
-          return [
-            {
-              source: "/service-worker.js",
-              destination: "/_next/static/service-worker.js",
-            },
-          ];
         },
       },
+    ],
+  },
+  experimental: {
+    async rewrites() {
+      return [
+        {
+          source: "/service-worker.js",
+          destination: "/_next/static/service-worker.js",
+        },
+      ];
     },
-  ],
-]);
+  },
+};
+
+const nextConfig = {};
+
+module.exports = withPlugins(
+  [withPurgeCss, [withOffline, offlineConfig]],
+  nextConfig
+);
